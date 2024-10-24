a4bfc234bf97c405dd21ca51cc012c926a4b95b6
Bryce Karnikar <brka@itu.dk>
2024-10-24 16:58:27 +0200

    Clients now handle own lamport time
    All functions in client.go have been updated to increment and/or compare lamport
    timestamps as required.

--- Chitty-Chat/client/client.go
+++ Chitty-Chat/client/client.go
@@ -30,14 +30,14 @@ func main() {
 	var localTime int64 = 0
 
 	// Join the chat
-	joinChat(client, &clientId, clientName)
+	joinChat(client, &clientId, clientName, &localTime)
 
 	// Start subscribing to messages in a separate goroutine
-	go subscribeToMessages(client)
+	go subscribeToMessages(client, &localTime)
 
 	// Publish a few messages
 	for i := 0; i < 3; i++ {
-		publishMessage(client, clientId, "Hello, ChittyChat!")
+		publishMessage(client, clientId, "Hello, ChittyChat!", &localTime)
 		time.Sleep(2 * time.Second)
 	}
 
@@ -45,9 +45,11 @@ func main() {
 	leaveChat(client, clientId)
 }
 
-func joinChat(client chittychat.ChittyChatClient, clientId *int64, clientName string) {
+func joinChat(client chittychat.ChittyChatClient, clientId *int64, clientName string, localTime *int64) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
+
+	*localTime++
 
 	joinResp, err := client.Join(ctx, &chittychat.ClientInfo{
 		ClientName: clientName,
@@ -56,29 +58,35 @@ func joinChat(client chittychat.ChittyChatClient, clientId *int64, clientName st
 		log.Fatalf("Failed to join chat: %v", err)
 	}
 
+	updateLamportTime(localTime, joinResp.LamportTime)
+
 	*clientId = joinResp.ClientId
 
 	log.Printf("Joined ChittyChat: %s", joinResp.WelcomeMessage)
-	log.Printf("Joined at Lamport time: %d", joinResp.LamportTime)
+	log.Printf("Joined at Lamport time: %d", *localTime)
 }
 
-func publishMessage(client chittychat.ChittyChatClient, clientId int64, content string) {
+func publishMessage(client chittychat.ChittyChatClient, clientId int64, content string, localTime *int64) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
+
+	*localTime++
 
 	pubResp, err := client.PublishMessage(ctx, &chittychat.ChatMessage{
 		ClientId:    clientId,
 		Content:     content,
-		LamportTime: time.Now().Unix(), // Could also use a real Lamport clock
+		LamportTime: *localTime,
 	})
 	if err != nil {
 		log.Fatalf("Failed to publish message: %v", err)
 	}
 
-	log.Printf("Message published at Lamport time: %d", pubResp.LamportTime)
+	updateLamportTime(localTime, pubResp.LamportTime)
+
+	log.Printf("Message published at Lamport time: %d", *localTime)
 }
 
-func subscribeToMessages(client chittychat.ChittyChatClient) {
+func subscribeToMessages(client chittychat.ChittyChatClient, localTime *int64) {
 	// Create an empty context for the subscription
 	stream, err := client.Subscribe(context.Background(), &emptypb.Empty{})
 	if err != nil {
@@ -91,14 +99,17 @@ func subscribeToMessages(client chittychat.ChittyChatClient) {
 		if err != nil {
 			log.Fatalf("Error receiving message: %v", err)
 		}
+		*localTime++
 		log.Printf("Received message from %d at Lamport time %d: %s",
 			msg.ClientId, msg.LamportTime, msg.Content)
 	}
 }
 
-func leaveChat(client chittychat.ChittyChatClient, clientId int64) {
+func leaveChat(client chittychat.ChittyChatClient, clientId int64, localTime *int64) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
+
+	*localTime++
 
 	leaveResp, err := client.Leave(ctx, &chittychat.ClientInfo{
 		ClientId: clientId,
@@ -107,9 +118,11 @@ func leaveChat(client chittychat.ChittyChatClient, clientId int64) {
 		log.Fatalf("Failed to leave chat: %v", err)
 	}
 
-	log.Printf("Left chat at Lamport time: %d", leaveResp.LamportTime)
+	updateLamportTime(localTime, leaveResp.LamportTime)
+
+	log.Printf("Left chat at Lamport time: %d", *localTime)
 }
 
 func updateLamportTime(local *int64, remote int64) {
-	*local = max(*local, remote)
+	*local = max(*local, remote) + 1
 }
